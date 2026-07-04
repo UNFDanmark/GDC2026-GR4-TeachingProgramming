c3b7f3414965ef9afc6da85f4f7e7b157bae4c09
GDC2026 Programming <gdc2026-programming@unf.dk>
2026-07-04 15:05:32 +0200

    opgave 3

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,23 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     public float movementSpeed;
-    [SerializeField] private float cooldown;
-    
+    [SerializeField] private float rotationSpeed;
+    private Rigidbody rb;
 
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rotationAction.Enable();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public InputAction rotationAction;
     private void Update()
     {
-        Debug.Log("Et eller andet");
+        float rotation = rotationAction.ReadValue<float>();
+        transform.Rotate( new Vector3(0, rotation * rotationSpeed, 0));
     }
 }
