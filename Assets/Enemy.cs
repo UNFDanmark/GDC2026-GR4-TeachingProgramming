c5f68c0bdbcb84c8584b1545b2d1d5ed4734a22f
GDC2026 Programming <gdc2026-programming@unf.dk>
2026-07-04 13:53:28 +0200

    opgave 1

---
+++ Assets/Enemy.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("Hej jeg er ond");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private void Update()
+    {
+        Debug.Log("Et eller andet");
+    }
+}
