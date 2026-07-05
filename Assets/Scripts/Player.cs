835ff429e2ed1d72dbd2ae33feb1363e7cb16730
GDC2026 Programming <gdc2026-programming@unf.dk>
2026-07-05 09:13:31 +0200

    Made movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,18 +6,23 @@ public class Player : MonoBehaviour
     public float movementSpeed;
     [SerializeField] private float rotationSpeed;
     private Rigidbody rb;
+    public InputAction rotationAction;
+    public InputAction movementAction;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
         rotationAction.Enable();
+        movementAction.Enable();
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    public InputAction rotationAction;
     private void Update()
     {
         float rotation = rotationAction.ReadValue<float>();
         rb.angularVelocity = rotation * rotationSpeed * Vector3.up;
+
+        Vector2 move = movementAction.ReadValue<Vector2>();
+        rb.linearVelocity = (transform.right * move.x + transform.forward * move.y) * movementSpeed * Time.deltaTime;
     }
 }
